280f645553ea2c7372c362e14bc090d481226501
Соня <203419427+Sonya-sama@users.noreply.github.com>
2025-06-04 22:52:22 +0300

    Update colors.lua

--- lua/kawaii/colors.lua
+++ lua/kawaii/colors.lua
@@ -20,6 +20,7 @@ colors.get_colors = function ()
         Cyan = "#B7D3DF",
         Blue = "#9EA1DE",
 
+        HotPink = "#FF1694",
         Taffy = "#FA86C4",
         BalletSlipper = "#F79AC0",
         Carnation = "#FFA6E9",
