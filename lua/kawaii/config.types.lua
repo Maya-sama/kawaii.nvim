abe24bf5de4b3bc3a3f3dd5e0f4d771ecee3e4fd
Соня <203419427+Sonya-sama@users.noreply.github.com>
2025-06-04 22:12:36 +0300

    Create the config documentation

---
+++ lua/kawaii/config.types.lua
@@ -0,0 +1,10 @@
+---@meta
+
+--- Configuration for Kawaii
+---@class kawaii.config
+--- Forbids Kawaii to change the background color
+---@param transparent boolean?
+--- Shall comments be rendered as italic
+---@param italic_comments boolean?
+--- Shall links (`Link' group) be underlined
+---@param underline_links boolean?
