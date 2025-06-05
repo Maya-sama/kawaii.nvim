dcc35e4341376f952e41dc342d254a92d5b07c18
Соня <203419427+Sonya-sama@users.noreply.github.com>
2025-06-05 17:08:38 +0300

    Create colorscheme for Lualine

---
+++ lua/lualine/themes/kawaii.lua
@@ -0,0 +1,39 @@
+local config = require("kawaii.config")
+local c = require("kawaii.colors").get_colors()
+
+return {
+    normal = {
+        a = { fg = c.Black, bg = c.White, gui = "bold" },
+        b = { fg = c.Blue, bg = c.RealBG },
+        c = { fg = c.White, bg = c.RealBG }
+    },
+    visual = {
+        a = { fg = c.Black, bg = c.Cyan, gui = "bold" },
+        b = { fg = c.NormalFG, bg = c.RealBG },
+    },
+    inactive = {
+        a = { fg = c.Black, bg = c.MidGray },
+        b = { fg = c.MidGray, bg = c.RealBG },
+        c = { fg = c.MidGray, bg = c.RealBG }
+    },
+    replace = {
+        a = { fg = c.Black, bg = c.Red, gui = "bold" },
+        b = { fg = c.NormalFG, bg = c.RealBG },
+        c = { fg = c.NormalFG, bg = c.RealBG }
+    },
+    insert = {
+        a = { fg = c.Black, bg = c.Yellow, gui = "bold" },
+        b = { fg = c.NormalFG, bg = c.RealBG },
+        c = { fg = c.NormalFG, bg = c.RealBG }
+    },
+    terminal = {
+        a = { fg = c.Black, bg = c.Green, gui = "bold" },
+        b = { fg = c.NormalFG, bg = c.RealBG },
+        c = { fg = c.NormalFG, bg = c.RealBG }
+    },
+    command = {
+        a = { fg = c.Black, bg = c.Blue, gui = "bold" },
+        b = { fg = c.NormalFG, bg = c.RealBG },
+        c = { fg = c.NormalFG, bg = c.RealBG }
+    }
+}
