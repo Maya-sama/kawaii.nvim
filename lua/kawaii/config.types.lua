8cc50517cf0b702591093ec59b462dd44a5c98c4
Соня <203419427+Sonya-sama@users.noreply.github.com>
2025-07-30 17:02:32 +0300

    Fux `config.types.lua'

--- lua/kawaii/config.types.lua
+++ lua/kawaii/config.types.lua
@@ -3,8 +3,8 @@
 --- Configuration for Kawaii
 ---@class kawaii.config
 --- Forbids Kawaii to change the background color
----@param transparent boolean?
+---@param transparent? boolean
 --- Shall comments be rendered as italic
----@param italic_comments boolean?
+---@param italic_comments? boolean
 --- Shall links (`Link' group) be underlined
----@param underline_links boolean?
+---@param underline_links? boolean
